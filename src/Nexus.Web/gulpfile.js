b2a0c700d832f6aa952d91b121b8e27d83081b62
ruvsdev <ruvs@dev>
2016-06-18 00:18:18 +0100

    Initial commit

---
+++ src/Nexus.Web/gulpfile.js
@@ -0,0 +1,43 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp'),
+    Q = require('q'),
+    rimraf = require('rimraf');
+
+gulp.task('clean', function (cb) {
+    return rimraf('./wwwroot/lib/', cb);
+});
+
+gulp.task('copy:lib', ['clean'], function () {
+    var libs = [
+        "@angular",
+        "systemjs",
+        "core-js",
+        "zone.js",
+        "reflect-metadata",
+        "rxjs"
+    ];
+
+    var promises = [];
+
+    libs.forEach(function (lib) {
+        var defer = Q.defer();
+        var pipeline = gulp
+            .src('node_modules/' + lib + '/**/*')
+            .pipe(gulp.dest('./wwwroot/lib/' + lib));
+
+        pipeline.on('end', function () {
+            defer.resolve();
+        });
+        promises.push(defer.promise);
+    });
+
+    return Q.all(promises);
+});
+
+//gulp.task('default', function () {
+//    // place code for your default task here
+//});
