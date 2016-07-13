5eac0f0728c236bbdfc192a8203db90d6ba89529
ruvsdev <ruvs@dev>
2016-07-13 21:56:29 +0100

    Add bootstrap, liteserver and test component

---
+++ src/Nexus.Web/scripts/app/participantLibrary/participant-library.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'mytest',
+    templateUrl: 'participantLibrary/participant-library.component.html'
+})
+export class ParticipantLibraryComponent { }
+
